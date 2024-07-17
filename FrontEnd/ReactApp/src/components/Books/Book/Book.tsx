e950a0d42ad55917a60a211ce0a08a6c28081e8c
invisible555 <niewidzialny51@gmail.com>
2024-07-17 19:00:21 +0200

    Update

--- FrontEnd/ReactApp/src/components/Books/Book/Book.tsx
+++ FrontEnd/ReactApp/src/components/Books/Book/Book.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./Book.module.css";
+import img from "../../../assets/images/react.svg";
 
 function Book() {
-  return <div className={styles.hotel}>book</div>;
+  return (
+    <div className={`row ${styles.book}`}>
+      <div className="col-4">
+        <img src={img} alt="" className="img-fluid" />
+      </div>
+      <div className="col-8">
+        <div className="row">
+          <div className="col">
+            <p>Tytuł</p>
+            <p>Miasto</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-12">
+        <p>Opis</p>
+        <a href="#" className="btn btn-primary">
+          Pokaż
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Book;
--- FrontEnd/ReactApp/src/components/Header/SearchBar/SearchBar.tsx
+++ FrontEnd/ReactApp/src/components/Header/SearchBar/SearchBar.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SerachBar() {
+  const [term, setTerm] = useState("");
+  const search = () => {
+    console.log("szukaj!", term);
+  };
+  const onKeyDownHandler = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
   return (
     <div className="d-flex">
-      <input className="form-control" type="text" placeholder="Szukaj..." />
-      <button className="btn btn-primary">Szukaj</button>
+      <input
+        value={term}
+        onKeyDown={onKeyDownHandler}
+        onChange={(e) => setTerm(e.target.value)}
+        className="form-control"
+        type="text"
+        placeholder="Szukaj..."
+      />
+      <button onClick={search} className="btn btn-primary">
+        Szukaj
+      </button>
     </div>
   );
 }
